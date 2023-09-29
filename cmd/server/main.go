5699b1d98431004f0a7dcb535276543c5bf2b124
andreev.y <andreev.y@n-t.io>
2023-09-29 04:58:38 +0300

    #1 added server and storage for metric

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,9 @@
 package main
 
-func main() {}
+import (
+	"github.com/andreevym/metric-collector/internal/server"
+)
+
+func main() {
+	server.StartServer()
+}
---
+++ internation_test/it_test.go
@@ -0,0 +1,51 @@
+package internation_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntegration(t *testing.T) {
+	t.Run("[good] valid url", func(t *testing.T) {
+		response, err := http.Post(
+			"http://localhost:8080/update/counter/someMetric/527",
+			"text/plain",
+			nil,
+		)
+		require.NoError(t, err)
+		require.NotNil(t, response)
+		require.Equal(t, http.StatusOK, response.StatusCode)
+	})
+	t.Run("[bad] wrong metric type", func(t *testing.T) {
+		response, err := http.Post(
+			"http://localhost:8080/update/UNKNOWN/someMetric/527",
+			"text/plain",
+			nil,
+		)
+		require.NoError(t, err)
+		require.NotNil(t, response)
+		require.Equal(t, http.StatusBadRequest, response.StatusCode)
+	})
+	t.Run("[bad] metric value didn't set", func(t *testing.T) {
+		response, err := http.Post(
+			"http://localhost:8080/update/counter/someMetric",
+			"text/plain",
+			nil,
+		)
+		require.NoError(t, err)
+		require.NotNil(t, response)
+		require.Equal(t, http.StatusNotFound, response.StatusCode)
+	})
+	t.Run("[bad] metric value didn't set", func(t *testing.T) {
+		response, err := http.Post(
+			"http://localhost:8080/update/a",
+			"text/plain",
+			nil,
+		)
+		require.NoError(t, err)
+		require.NotNil(t, response)
+		require.Equal(t, http.StatusNotFound, response.StatusCode)
+	})
+}
