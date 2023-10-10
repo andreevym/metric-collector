13612fa26c571742fdd71bf235f127935b6ee8d6
andreev.y <andreev.y@n-t.io>
2023-10-10 15:14:40 +0300

    # 4 add address env for server

---
+++ cmd/server/env.go
@@ -0,0 +1,5 @@
+package main
+
+type EnvConfig struct {
+	Address string `env:"ADDRESS"`
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	"github.com/andreevym/metric-collector/internal/server"
+	"github.com/caarlos0/env"
 )
 
 func main() {
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
+
+	var config EnvConfig
+	err := env.Parse(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if config.Address != "" {
+		flagRunAddr = config.Address
+	}
 
 	server.StartServer(flagRunAddr)
 }
