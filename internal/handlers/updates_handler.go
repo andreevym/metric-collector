30aab4b5c9be124b05de897d0b5ee0241c5d1de2
andreev.y <andreev.y@n-t.io>
2023-12-13 23:15:32 +0300

    # 13 move save all metrics to storage pkg

--- internal/handlers/updates_handler.go
+++ internal/handlers/updates_handler.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
 	"io"
 	"net/http"
 
@@ -33,45 +32,5 @@ func (s ServiceHandlers) PostUpdatesHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	if len(metrics) == 0 {
-		return
-	}
-
-	result := map[string]*storage.Metric{}
-	for _, metric := range metrics {
-		found, ok := result[metric.ID+metric.MType]
-		if ok && found != nil && metric.MType == storage.MTypeCounter {
-			newDelta := *metric.Delta + *found.Delta
-			metric.Delta = &newDelta
-		}
-
-		result[metric.ID+metric.MType] = metric
-	}
-
-	metricsR := map[string]storage.MetricR{}
-	for _, metric := range metrics {
-		found, err := s.storage.Read(r.Context(), metric.ID, metric.MType)
-		if err != nil && !errors.Is(err, storage.ErrValueNotFound) {
-			logger.Logger().Error("failed update metric",
-				zap.Error(err))
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if found != nil && metric.MType == storage.MTypeCounter {
-			newDelta := *metric.Delta + *found.Delta
-			metric.Delta = &newDelta
-		}
-
-		metricsR[metric.ID+metric.MType] = storage.MetricR{
-			Metric:   metric,
-			IsExists: found != nil,
-		}
-	}
-
-	err = s.storage.CreateAll(r.Context(), metricsR)
-	if err != nil {
-		logger.Logger().Error("err", zap.Error(err))
-		w.WriteHeader(http.StatusBadRequest)
-	}
+	storage.SaveAllMetric(r.Context(), s.storage, metrics)
 }
