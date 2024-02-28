8ef7abdcaf740d26d110ad0f8a7ef022f29fef4c
Yury Andreev <andreevym@gmail.com>
2024-02-28 09:24:08 +0300

    # 18 write go doc for cmd
    Signed-off-by: Yury Andreev <andreevym@gmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,5 @@
+// Package main is the entry point of the Metric Collector application.
+// It initializes configurations, logging, and starts the server.
 package main
 
 import (
@@ -8,17 +10,22 @@ import (
 	"github.com/andreevym/metric-collector/internal/server"
 )
 
+// main is the entry point of the application.
+// It initializes configurations, logging, and starts the server.
 func main() {
+	// Initialize server configurations.
 	cfg := config.NewServerConfig().Init()
 	if cfg == nil {
 		log.Fatal("server config can't be nil")
 	}
 
+	// Initialize logger.
 	_, err := logger.NewLogger(cfg.LogLevel)
 	if err != nil {
-		log.Fatal("logger can't be init", cfg.LogLevel, err)
+		log.Fatal("logger can't be initialized:", cfg.LogLevel, err)
 	}
 
+	// Start the server.
 	err = server.Start(
 		cfg.DatabaseDsn,
 		cfg.FileStoragePath,
